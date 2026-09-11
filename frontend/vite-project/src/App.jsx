ec97b3c53ca2587ceeac9d6cc761577214b107bc
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-09-11 15:31:20 +0530

    file changed

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Routes, Route,Navigate} from "react-router-dom";
-import AuthPage from "./pages/AuthPage.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import AuthPage from "./pages/Authpage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import HomePage from "./pages/Homepage.jsx";
 import StatsUI from "./components/Stats.jsx";
@@ -9,24 +9,23 @@ import LeaderboardUI from "./components/Leaderboard.jsx";
 import MultiplayerMode from "./components/MultiplayerMode.jsx"
 import { useGameStore } from "./store/Gamestore.js";
 import { Toaster } from "react-hot-toast";
-import userAuthStore  from "./store/AuthenticationStore.js";
+import userAuthStore from "./store/AuthenticationStore.js";
 import ProfileDropDown from "./components/ProfileDropDown.jsx";
 import UserDetails from "./components/userdetails.jsx";
 
 function App() {
- 
-  const {userauth,checkauth}=userAuthStore();
 
-  useEffect(()=>
-  {
+  const { userauth, checkauth } = userAuthStore();
+
+  useEffect(() => {
     checkauth();
-  },[])
-  
+  }, [])
+
   const [refreshKey, setRefreshKey] = useState(0);
-   const {
+  const {
     resetGame
   } = useGameStore();
-  
+
   const handlePracticeClick = () => {
     setRefreshKey((prev) => prev + 1);
     resetGame()
@@ -38,11 +37,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage refreshKey={refreshKey} />} />
         <Route path="/auth" element={!userauth ? <AuthPage /> : <Navigate to="/" />} />
-        <Route path="/multiplayer" element={userauth ? <MultiplayerMode/> : <Navigate to="/auth"/>}/>
-        <Route path="/profile" element={userauth ? <ProfileDropDown/> : <Navigate to="/auth" />} />
-        <Route path="/stats" element={userauth ? <StatsUI/> : <Navigate to="/auth" />} />
-        <Route path="/userdetails" element={userauth ? <UserDetails/> : <Navigate to="/auth" />} />
-        <Route path="/leaderboard" element={<LeaderboardUI/>}/>
+        <Route path="/multiplayer" element={userauth ? <MultiplayerMode /> : <Navigate to="/auth" />} />
+        <Route path="/profile" element={userauth ? <ProfileDropDown /> : <Navigate to="/auth" />} />
+        <Route path="/stats" element={userauth ? <StatsUI /> : <Navigate to="/auth" />} />
+        <Route path="/userdetails" element={userauth ? <UserDetails /> : <Navigate to="/auth" />} />
+        <Route path="/leaderboard" element={<LeaderboardUI />} />
       </Routes>
       <Toaster
         toastOptions={{
